e05b396b469f116960d70c170dc63482aa8b9bfe
toto <toto@gmail.com>
2025-09-29 03:23:25 +0700

    update Login page 1

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -7,25 +7,40 @@ export default function Login({ setIsLoggedIn }) {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
+  // ----------------------------------------
+  // handleSubmit
+  // ยังไม่ได้แก้นะตรงนี้
   const handleSubmit = (e) => {
     e.preventDefault();
     // สมมุติ login ง่าย ๆ
-    if(username === "admin" && password === "1234") {
+    if (username === "admin" && password === "1234") {
       setIsLoggedIn(true);
-      navigate("/"); // ไปหน้า Home
+      navigate("/");
     } else {
       alert("username หรือ password ผิด");
     }
   };
-
+  // ------------------------------------------
+  // render
   return (
-    <div className="alingn-center">
-      <h1 className="text-2xl font-bold">Login Page</h1>
-      <form onSubmit={handleSubmit}>
-        <input className="border border-purple-200" placeholder="Username" value={username} onChange={e=>setUsername(e.target.value)} />
-        <input placeholder="Password" type="password" value={password} onChange={e=>setPassword(e.target.value)} />
-        <button type="submit">Login</button>
-      </form>
+    <div className="min-h-screen flex items-center justify-center bg-[#161616] text-white">
+      <div className="w-full max-w-sm bg-[#1a1a1a] rounded p-10 pt-0 shadow-lg">
+        <img src="src/assets/myhis-img.png" alt="logo" className="mx-auto w-80 h-80 bg-transparent"/>
+        <h1 className="text-3xl font-bold text-center mb-10 mt-0">Sing In</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+           <div className="flex flex-col">
+            {/* ค่อยทำ onclick ที่หลัง */}
+            <label htmlFor="username" className="mb-1.5 text-gray-300 text-m">ชื่อผู้ใช้</label> 
+          <input className="block w-full h-10 px-3 rounded bg-[#747474]" placeholder="กรอกชื่อผู้ใช้" value={username} onChange={e => setUsername(e.target.value)} />
+          </div>
+          <div className="flex flex-col">
+            {/* ค่อยทำ onclick ที่หลัง */}
+            <label htmlFor="password" className="mb-1.5 text-gray-300 text-m">รหัสผ่าน</label>
+          <input className="block w-full h-10 px-3 rounded bg-[#747474]" placeholder="กรอกรหัสผ่าน" type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          </div>
+          <button type="submit" className="block w-30 h-10 px-2 mt-5 text-sm rounded-2xl text-white mx-auto bg-[#575757] hover:bg-[#00ff6a] transition-colors duration-300">เข้าสู่ระบบ</button>
+        </form>
+      </div>
     </div>
   );
 }
